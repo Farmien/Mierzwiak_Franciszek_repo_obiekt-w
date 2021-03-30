73070d4d6c09479d0d3d11c4a13f988899f2b4bf
Farmien <franula98765@gmail.com>
2021-03-30 09:39:53 +0200

    1 i 2 zadanka :>

---
+++ TworzenieObiektow/Logowanie.java
@@ -0,0 +1,14 @@
+class Logowanie{
+	private String login = "";
+	private String haslo = "";
+	
+	public Logowanie(String login, String haslo){
+		this.login = login;
+		this.haslo = haslo;
+	}
+	
+	@Override
+	public String toString(){
+		return "login: "+login+" Password: "+haslo;
+	}
+}
