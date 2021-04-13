907d1097622a5716dfc7ffb0a485770215fba385
Farmien <franula98765@gmail.com>
2021-04-13 10:06:50 +0200

    13.04.2021 Dodanie pola trojkata rownobocznego

--- TrojkatyZadania/Main.java
+++ TrojkatyZadania/Main.java
@@ -3,5 +3,6 @@ class Main{
 	
 	public static void main(String[] args){
 		Triangle troj1 = new Triangle(10, 5);
+		Triangle troj2 = new Triangle(5);
 	}
 }
--- TrojkatyZadania/Triangle.java
+++ TrojkatyZadania/Triangle.java
@@ -1,8 +1,13 @@
 
 class Triangle{
 	
+	//pole trojkata(podstawa * wysokosc)
 	public Triangle(int wysokosc, int podstawa){
 		System.out.println(wysokosc*podstawa/2);
 	}
-
+	
+	//pole trojkata rownobocznego
+	public Triangle(float podstawa){
+		System.out.println(podstawa*podstawa*1.73205080757/4);
+	}
 }
